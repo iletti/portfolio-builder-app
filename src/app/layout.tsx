a27ae521227bd45aedf5f0f11cb211d9a097fe38
Ilari Schmidt <92219610+iletti@users.noreply.github.com>
2024-05-16 15:50:40 +0300

    sa

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,18 @@
-
+import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
+export const metadata: Metadata = {
+  title: "Ilari Schmidt - Portfolio",
+  description: "Tutustu Ilari Schmidtin osaamiseen ja toteuttamiin projekteihin.",
+  author: "Ilari Schmidt",
+  keywords: ["Ilari Schmidt", "portfolio", "digistrategia", "digimarkkinointi", "web kehitys", "analytiikka", "CRM", "sales development"],
+  image: "/images/portfolio-preview.jpg",
+  url: "https://www.portfolio.ilarischmidt.com/",
+  type: "website",
+};
 
 export default function RootLayout({
   children,
@@ -11,7 +20,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="fi">
       <body className={inter.className}>{children}</body>
     </html>
   );
